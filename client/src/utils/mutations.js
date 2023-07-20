a4ad2651f6921e2eb3da18385a88e33424ae9233
Christopher Peret <cperet89@gmail.com>
2023-07-19 20:47:32 -0700

    added login to mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,27 @@
 import { gql } from '@apollo/client';
 
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+                bookCount
+                savedBooks {
+                    bookId
+                    authors
+                    title
+                    description
+                    image
+                    link
+                }
+            }
+        }
+    }
+`;
+
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
