d6b25b690c654f942d61275835dd943b2f2c52c9
Christopher Peret <cperet89@gmail.com>
2023-07-19 18:43:20 -0700

    resolvers.js rough in

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,31 @@
+const {user} = require('../models');
+const {AuthenticationError} = require('apollo-server-express');
+const {signToken} = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if(context.user) {
+                const userData = await User.findOne({_id: context.user._id})
+                .select('-__v -password')
+                .populate('books');
+
+                return userData;
+            }
+
+            throw new AuthenticationError('Not logged in');
+        }
+    },
+
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return{token, user};
+        }
+    }
+};
+
+
+module.exports = resolvers;
